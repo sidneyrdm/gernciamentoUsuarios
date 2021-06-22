c49737667ea34fd8b33bb06fc82c63c00a8f6514
sidneyrdm <sidneybritomal@gmail.com>
2021-06-22 17:53:22 -0300

    implementação da parte de exclusão de usuário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -214,7 +214,7 @@ class UserController {
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
         `;
@@ -228,6 +228,15 @@ class UserController {
     }
 
     addEventsTr(tr) {
+
+        tr.querySelector(".btn-delete").addEventListener("click", e => {
+
+            if (confirm("Deseja realmente excluir este usuário?")) {
+                tr.remove();
+                this.updateCount();
+            }
+
+        });
 
         tr.querySelector(".btn-edit").addEventListener("click", e => {
 
