c7b1fcdf2276be17a4d0ea591d608639f19a8dca
sidneyrdm <sidneybritomal@gmail.com>
2021-06-22 19:01:56 -0300

    mudança de armazenamento dos dados de seeionStorage para localStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -205,9 +205,9 @@ class UserController {
     getUsersStorage() {
         let users = [];
 
-        if (sessionStorage.getItem("users")) {
+        if (localStorage.getItem("users")) {
 
-            users = JSON.parse(sessionStorage.getItem("users"));
+            users = JSON.parse(localStorage.getItem("users"));
 
         }
         return users;
@@ -234,7 +234,8 @@ class UserController {
 
         users.push(data);
 
-        sessionStorage.setItem("users", JSON.stringify(users));
+        //sessionStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("users", JSON.stringify(users));
     }
 
     addLine(dataUser) {
